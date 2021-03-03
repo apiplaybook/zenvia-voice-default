5248875806a147139a792859c5ebcfcf4a5ee965
Henry Kimura <htkimura@gmail.com>
2021-03-03 17:22:15 -0300

    feat: add server config file

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import cron from 'node-cron'
+import { checkAndSendSMSUseCase } from './UseCases/CheckAndSendSMS/index'
+
+// A cada hora será feita uma checagem no banco de dados se há a necessidade de enviar SMS de voz, caso tiver, será enviado
+cron.schedule('* * * *', () => {
+	checkAndSendSMSUseCase.handle()
+})
